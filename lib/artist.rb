07aaf9bc745f8551e5bf785d673d1c583901a812
Tymeckia Kendall <tykend@gmail.com>
2019-03-24 22:25:53 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,14 +11,15 @@ class Artist
     @@song_count
   end
   
-    def songs
-    @songs
-  end
   
   def add_song (song)
     @songs << song
     song.artist = self
        @@song_count +=1
+  end
+  
+  def songs
+    @songs
   end
   
   def add_song_by_name(name)
